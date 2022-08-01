3cf63551fe4b3dcf48da5897ac926b633155ee5f
Rosalind <rosalindjchang@gmail.com>
2022-07-31 23:12:16 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+
+    $(".laimg").hide();
+    $("#la").hover(function(){
+        $(".laimg").show();
+    },
+    function(){
+        $(".laimg").hide();
+    }
+    );
+    
+    $("#instrumentlogo").hide();
+    $("#instrument").hover(function(){
+        $("#instrumentlogo").show();
+    },
+    function(){
+        $("#instrumentlogo").hide();
+    }
+    );
+    
+    $("#bookimg").hide();
+    $("#book").hover(function(){
+        $("#bookimg").show();
+    },
+    function(){
+        $("#bookimg").hide();
+    }
+    );
+    
+     $("#hairimg").hide();
+    $("#pixie").hover(function(){
+        $("#hairimg").show();
+    },
+    function(){
+        $("#hairimg").hide();
+    }
+    );
+    
+     $("#blobimg").hide();
+    $("#blob").hover(function(){
+        $("#blobimg").show();
+    },
+    function(){
+        $("#blobimg").hide();
+    }
+    );
+    
+     $("#okimg").hide();
+    $("#ok").hover(function(){
+        $("#okimg").show();
+    },
+    function(){
+        $("#okimg").hide();
+    }
+    );
+    
+      $("#fineimg").hide();
+    $("#fine").hover(function(){
+        $("#fineimg").show();
+    },
+    function(){
+        $("#fineimg").hide();
+    }
+    );
+    
+      $("#afterhoursimg").hide();
+    $("#afterhours").hover(function(){
+        $("#afterhoursimg").show();
+    },
+    function(){
+        $("#afterhoursimg").hide();
+    }
+    );
+    
+      $("#pixelimg").hide();
+    $("#pixel").hover(function(){
+        $("#pixelimg").show();
+    },
+    function(){
+        $("#pixelimg").hide();
+    }
+    );
+    
+      $("#spaceimg").hide();
+    $("#spacejanitor").hover(function(){
+        $("#spaceimg").show();
+    },
+    function(){
+        $("#spaceimg").hide();
+    }
+    );
+    
+    
+});
