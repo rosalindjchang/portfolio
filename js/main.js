5da98eca959fa601cfef1bf1cfbfd2ac636f7b4e
Rosalind <rosalindjchang@gmail.com>
2022-08-01 17:12:19 -0700

    updatee categories added

--- js/main.js
+++ js/main.js
@@ -38,55 +38,89 @@ $(document).ready(function(){
     
      $("#blobimg").hide();
     $("#blob").hover(function(){
+        $(".blobcategory").addClass("blackborder");
         $("#blobimg").show();
     },
     function(){
         $("#blobimg").hide();
+        $(".blobcategory").removeClass("blackborder");
     }
     );
     
      $("#okimg").hide();
     $("#ok").hover(function(){
         $("#okimg").show();
+        $(".okcategory").addClass("blackborder");
     },
     function(){
         $("#okimg").hide();
+        $(".okcategory").removeClass("blackborder");
     }
     );
     
       $("#fineimg").hide();
     $("#fine").hover(function(){
+        $(".finecategory").addClass("blackborder");
         $("#fineimg").show();
     },
     function(){
         $("#fineimg").hide();
+        $(".finecategory").removeClass("blackborder");
     }
     );
     
       $("#afterhoursimg").hide();
     $("#afterhours").hover(function(){
+        $(".aftercategory").addClass("blackborder");
         $("#afterhoursimg").show();
     },
     function(){
         $("#afterhoursimg").hide();
+        $(".aftercategory").removeClass("blackborder");
     }
     );
     
       $("#pixelimg").hide();
     $("#pixel").hover(function(){
+        $(".pixelcategory").addClass("blackborder");
         $("#pixelimg").show();
     },
     function(){
         $("#pixelimg").hide();
+        $(".pixelcategory").removeClass("blackborder");
     }
     );
     
       $("#spaceimg").hide();
     $("#spacejanitor").hover(function(){
+        $(".spacecategory").addClass("blackborder");
         $("#spaceimg").show();
     },
     function(){
+        $(".spacecategory").removeClass("blackborder");
         $("#spaceimg").hide();
+    }
+    );
+    
+      $("#lacfimg").hide();
+    $("#lacf").hover(function(){
+        $(".lacfcategory").addClass("blackborder");
+        $("#lacfimg").show();
+    },
+    function(){
+        $(".lacfcategory").removeClass("blackborder");
+        $("#lacfimg").hide();
+    }
+    );
+    
+       $("#ucla4allimg").hide();
+    $("#ucla4all").hover(function(){
+        $(".uclacategory").addClass("blackborder");
+        $("#ucla4allimg").show();
+    },
+    function(){
+        $(".uclacategory").removeClass("blackborder");
+        $("#ucla4allimg").hide();
     }
     );
     
