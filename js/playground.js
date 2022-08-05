e10dd01050658a35a0ef631a572b3b43738b49d7
Rosalind <rosalindjchang@gmail.com>
2022-08-05 16:18:34 -0700

    playground update

---
+++ js/playground.js
@@ -0,0 +1,12 @@
+
+
+
+
+let sketch = function(p) {
+    p.setup = function(){
+      p.createCanvas(200, 200);
+      p.background(0);
+    }
+  };
+  new p5(sketch, window.document.getElementById('section1'));
+
