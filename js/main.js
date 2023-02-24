83f658b995166eedf08ed22050ffb53a1fe303d6
Rosalind <rosalindjchang@gmail.com>
2023-02-23 16:47:08 -0800

    loading

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+    $(document).ready(function() {
+  document.getElementsByTagName("html")[0].style.visibility = "visible";
+});
     $(".laimg").hide();
     $("#la").hover(function(){
         $(".laimg").show();
